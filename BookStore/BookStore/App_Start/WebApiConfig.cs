541287308b268a04c4b96d20e7e02a0a49548c51
Orkhan Alikhanov <Orkhan.Alikhanov@gmail.com>
2016-08-04 14:14:18 +0400

    Added {action}

--- BookStore/BookStore/App_Start/WebApiConfig.cs
+++ BookStore/BookStore/App_Start/WebApiConfig.cs
@@ -16,7 +16,7 @@ namespace BookStore
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
