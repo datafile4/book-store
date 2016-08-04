afb17109a11005d15e797201d4cdf2fc4427bfc9
Emil Alasgarov <debianmik@gmail.com>
2016-08-04 16:20:03 +0400

    Added User and Books models. Cart renamed to Card.

--- BookStore/BookStore/Models/Card.cs
+++ BookStore/BookStore/Models/Card.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.SqlClient;
 
 namespace BookStore.Models
 {
-    public class Cart
+    public class Card
     {
-        public int CartId { get; set; }
-        public string ProductId { get; set; }
+        [Key]        
+        public int ID { get; set; }        
+
+        [Display(Name = "Book")]
+        public string BookID { get; set; }
+        [ForeignKey("BookID")]
+        public virtual Book Books { get; set; } //fetches primary keys
+
+
         //we keep track on the date, not the date or time
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}",ApplyFormatInEditMode = true)]
--- BookStore/BookStore/Models/Genre.cs
+++ BookStore/BookStore/Models/Genre.cs
@@ -6,10 +6,10 @@ using System.ComponentModel.DataAnnotations;
 
 namespace BookStore.Models
 {
-    public class Lang
+    public class Genre
     {
         [Key]
         public int ID { get; set; }
-        public string Name { get; set; }        
+        public string Name { get; set; }
     }
 }
