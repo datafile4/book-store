c628d3ed2cf0ef264fb8979b7e019ac9b23c5d68
NadirHasimov <hasimov.nadir@yandex.com>
2016-08-05 11:46:08 +0400

    Register Model created.

--- BookStore/BookStore/Models/UserManagerModels.cs
+++ BookStore/BookStore/Models/UserManagerModels.cs
@@ -11,5 +11,16 @@ namespace BookStore.Models
         public string Password { get; set; }
 
     }
+    public class RegisterModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string CartId { get; set; }
+
+    }
+
 
 }
