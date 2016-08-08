3218737e7deb15f7ac8635cf2d5b19b46a2c287c
Orkhan Alikhanov <Orkhan.Alikhanov@gmail.com>
2016-08-09 00:33:09 +0400

    Fixed casing.

--- BookStore/BookStore/Models/UserManagerModels.cs
+++ BookStore/BookStore/Models/UserManagerModels.cs
@@ -15,7 +15,7 @@ namespace BookStore.Models
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string UserName { get; set; }
+        public string Username { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
 
