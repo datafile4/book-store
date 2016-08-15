ea8bacdf32dee32d830db361b39832facdd08186
NadirHasimov <hasimov.nadir@yandex.com>
2016-08-15 12:51:27 +0400

    Added model validation attributes

--- BookStore/BookStore/Models/Book.cs
+++ BookStore/BookStore/Models/Book.cs
@@ -8,14 +8,23 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace BookStore.Models
 {
     public class Book
-    {        
+    {
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Author { get; set; }
+        [Url]
+        [Required]
         public string ImageURL { get; set; }
-        public  decimal Price { get; set; }
-        public int GenreID { get; set; }        
-        public int LanguageID { get; set; }
-        public int UserID { get; set; }
+        [Required]
+        [Range(0,1000)]
+        public decimal? Price { get; set; }
+        [Required]
+        public int? GenreID { get; set; }
+        [Required]
+        public int? LanguageID { get; set; }
+        [Required]
+        public int? UserID { get; set; }
         
     }
 }
--- BookStore/BookStore/Models/UserManagerModels.cs
+++ BookStore/BookStore/Models/UserManagerModels.cs
@@ -2,20 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace BookStore.Models
 {
     public class LoginModel
     {
+        [Required]
         public string Username { get; set; }
+        [Required]
         public string Password { get; set; }
     }
     public class RegisterModel
     {
+
+        [Required(ErrorMessage = "First name is required!")]
         public string FirstName { get; set; }
+
+
+        [Required(ErrorMessage = "Last name is required!")]
         public string LastName { get; set; }
+
+        [Required (ErrorMessage ="Username is required!")]
+        [StringLength(20, MinimumLength = 5, ErrorMessage = "Username must be at least 5 characters long.")]
         public string Username { get; set; }
+
+
+        [Required(ErrorMessage = "The email address is required!")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage ="Password is required!")]
+        [StringLength(30, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long.")]
         public string Password { get; set; }
     }
 
@@ -26,7 +44,6 @@ namespace BookStore.Models
         public string Username { get; set; }
         public string Email { get; set; }
     }
-
     public class BookModel
     {
         public string Name { get; set; }
