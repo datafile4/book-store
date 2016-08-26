307fd3a0a5cf97eeb6847a601b4a8924a322530e
Amiraslan <aslanbaxisli.ba@gmail.com>
2016-08-26 11:21:44 +0400

    Matched method Name

--- BookStore/BookStore/Scripts/GetBooks.js
+++ BookStore/BookStore/Scripts/GetBooks.js
@@ -1,19 +1,15 @@
 ﻿app.controller('GetBooks', function ($scope, $http) {
 
     console.log("Geldiiii 1 \n");
-    var res = $http.get("api/bookstore/GetAllBooks");
+    var res = $http.post("api/bookstore/GetBooks", { PageNumber : 0, PageLength : 10});
     console.log("Geldiiii 2 \n");
     res.success(function (response) {
-        console.log("Geldiiii ela \n");
         $scope.Books = response;
-        console.log(JSON.stringify(response));
     });
     res.error(function (response) {
-        console.log("Geldiiii berbat \n");
-        console.log("In controller !!" + JSON.stringify(response));
+       
+        console.log("In controller error!!" + JSON.stringify(response));
     })
-
-
 
 
     var f = $scope;
