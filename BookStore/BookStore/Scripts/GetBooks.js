cd8b0f2e9d2e11d1bed374476b36d15caf659f42
Amiraslan <aslanbaxisli.ba@gmail.com>
2016-08-26 10:50:03 +0400

    Added Html Css

--- BookStore/BookStore/Scripts/GetBooks.js
+++ BookStore/BookStore/Scripts/GetBooks.js
@@ -1,16 +1,55 @@
 ﻿app.controller('GetBooks', function ($scope, $http) {
-    
+
     console.log("Geldiiii 1 \n");
     var res = $http.get("api/bookstore/GetAllBooks");
     console.log("Geldiiii 2 \n");
     res.success(function (response) {
         console.log("Geldiiii ela \n");
         $scope.Books = response;
-        console.log( JSON.stringify(response));
+        console.log(JSON.stringify(response));
     });
     res.error(function (response) {
         console.log("Geldiiii berbat \n");
         console.log("In controller !!" + JSON.stringify(response));
     })
-    
+
+
+
+
+    var f = $scope;
+
+
+    $http.get('api/BookStore/GetLanguages').success(function (data) {
+        f.Languages = data
+    });
+    $http.get('api/BookStore/GetGenres').success(function (data) {
+        f.Genres = data
+    });
+
+    f.upload = function () {
+
+        var uploadData = {
+            Name: f.bookName,
+            Author: f.Author,
+            ImageURL: f.ImageURL,
+            LanguageID: f.SelectedLang.ID,
+            GenreID: f.SelectedGenre.ID,
+            Price: f.Price
+        }
+
+        $http.post('api/BookStore/UploadBook', uploadData)
+        .then(
+        function (response) {
+            vm.message = response.data.message,
+            vm.messageStyle = { color: (response.data.success ? 'green' : 'red') };
+        },
+
+        function (reason) {
+
+            console.log(reason);
+        })
+
+    }
+
+
 })
